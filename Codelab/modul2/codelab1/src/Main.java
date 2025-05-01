a5af8e20932b5df95244cc95297e2637f4a9b444
faulfl <faullll76@gmail.com>
2025-05-01 23:28:11 +0700

    cd1

---
+++ Codelab/modul2/codelab1/src/Main.java
@@ -0,0 +1,16 @@
+// Kelas Utama (Main) yang akan menjalankan program.
+public class Main {
+    public static void main(String[] args) {
+        //Membuat objek hewan1 dan hewan 2 dengan nama, jenis, dan suara
+        Hewan hewan1 = new Hewan("singa", "Mamalia", "rawrrr");
+
+        Hewan hewan2 = new Hewan("Anjing", "Mamalia", "pinjam 100k");
+
+        // Memanggil metode tampilkanInfo() untuk menampilkan informasi hewan1
+        hewan1.tampilkanInfo();
+        // Memanggil metode tampilkanInfo() untuk menampilkan informasi hewan2
+        hewan2.tampilkanInfo();
+    }
+}
+
+//Muhamad Robi Ardita_202410370110002_2B
